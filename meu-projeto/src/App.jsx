d1d34e5312aed571c070f01168e1aa76fd370c73
DevigMurilo <igormurilo.ac.21@gmail.com>
2026-08-27 20:32:15 -0300

    so o basico , agora vamos para um exemplo pratico

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -1,8 +1,10 @@
-import {Lista} from './components/Lista.jsx'
-import array from './components/Lista.jsx'
-import { Descricao } from './componets/Descricao.jsx'
-import {Section} from './componets/section.jsx'
+import { Descricao } from './components/Descricao.jsx'
+import {Section} from './components/section.jsx'
+import useEffect from 'react'
 
+useEffect(() => {
+  console.log('App.jsx foi montado')
+}, [])
 
 
 export default function App() {
@@ -10,7 +12,8 @@ export default function App() {
     <>
         <Section />
         <Descricao />
-        <Lista array={array} />
+        <useEffect />
     </>
   )
 }
+// 
--- meu-projeto/src/components/Descricao.jsx
+++ meu-projeto/src/components/Descricao.jsx
@@ -2,4 +2,7 @@ export function Descricao() {
     return (
         <p>my first project with react</p>
     )
+
 }
+
+export default Descricao;
