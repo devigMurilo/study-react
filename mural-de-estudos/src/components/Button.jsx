35c9404a437b7191046557a027aa602c3696450b
DevigMurilo <igormurilo.ac.21@gmail.com>
2026-09-15 23:17:41 -0300

    style: add margin to button in Botao component

--- mural-de-estudos/src/components/Button.jsx
+++ mural-de-estudos/src/components/Button.jsx
@@ -2,7 +2,7 @@ import { Button } from 'react-bootstrap'
 
 export default function Botao({ }) {
   return (
-    <button type="button" class="d-grid gap-2 col-6 mx-auto btn btn-primary">view users</button>
+    <button type="button" class="d-grid gap-2 col-6 mx-auto btn btn-primary my-4">view users</button>
   );
 }
 
